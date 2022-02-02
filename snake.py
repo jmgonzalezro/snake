e8944aaef42dd2584d8fe1545c00f773a6a229c2
jogonzro <jmgonzalezro@gmail.com>
2022-02-02 22:34:45 +0100

    some changes on tkinter and booster spawn

--- snake.py
+++ snake.py
@@ -1,13 +1,31 @@
 from random import random
+import sys
 import tkinter as tk
-
 
 window = tk.Tk()
 
 window_dimensions = [800, 800]
-window.geometry(str(window_dimensions[0]) + 'x' + str(window_dimensions[1]))
+window.geometry(str(window_dimensions[0]) + "x" + str(window_dimensions[1]))
 window.resizable(0, 0)
 window.after_cancel
+
+window.title("Snake over 9000 turbo'")
+
+window.protocol("WM_DELETE_WINDOW", sys.exit)
+
+frames_per_second = 12
+
+game_canvas = tk.Canvas(window,
+                        width=window_dimensions[0],
+                        height=window_dimensions[1],
+                        bd=0,
+                        highlightthickness=0)
+
+game_canvas.pack()
+
+game_scale = 25
+game_dimensions = [window_dimensions[0] / game_scale,
+                   window_dimensions[1] / game_scale]
 
 
 class Screen:
@@ -25,16 +43,21 @@ class input:
 
 
 class Snake:
+    # spawn position
     # body
     # head
     # movement
     pass
 
 
-class Booster:
+def create_booster():
+    # spawn position
+    spawn_position = [random.randit(0, window_dimensions[0]-1), 
+                      random.randint(0, window_dimensions[1]-1)]
+    # need to check if it's spawning in the actual snake
     # size
     # power_up
-    pass
+    return spawn_position
 
 
 class App(tk.Frame):
