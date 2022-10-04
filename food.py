9235d7289b9a06eaa018bac39c371a07308d7901
jogonzro <jmgonzalezro@gmail.com>
2022-10-04 15:31:11 +0200

    added scoreboard and game_over

--- food.py
+++ food.py
@@ -1,15 +1,19 @@
 from random import randint
 from turtle import Turtle
 
+
 class Food(Turtle):
 
     def __init__(self):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len=8.5, stretch_wid=8.5)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
-        random_x = randint(-200,200)
-        random_y = randint(-200,200)
+        self.refresh()
+
+    def refresh(self):
+        random_x = randint(-280,280)
+        random_y = randint(-280,280)
         self.goto(random_x, random_y)
